58465887e611a1de280a2b211d9175444e762bd2
Danny Tang <tang_daniel@outlook.com>
2023-06-03 18:32:56 -0700

    added blog page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          I a subdomain!!!
+          Blog Home Page
         </h1>
 
         <p className={styles.description}>
